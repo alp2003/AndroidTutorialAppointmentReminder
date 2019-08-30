3ddf15c493f7f3fcc68d3549ede4b6ae2c19eed4
Alex Pekker <alp2003@gmail.com>
2019-08-30 21:55:01 +0300

    onAddAppointmentButton
    Pass Appointment data to MainActivity

--- app/src/main/java/com/android/androidtutorialappointmentreminder/AddAppointmentActivity.java
+++ app/src/main/java/com/android/androidtutorialappointmentreminder/AddAppointmentActivity.java
@@ -5,10 +5,15 @@ import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.Calendar;
@@ -19,6 +24,8 @@ public class AddAppointmentActivity extends AppCompatActivity {
 
     private TextView mTextViewDate;
     private TextView mTextViewTime;
+    private EditText mEditTextName;
+    private Spinner mSpinnerTaskType;
 
 
 
@@ -77,6 +84,8 @@ public class AddAppointmentActivity extends AppCompatActivity {
     private void initControls() {
         mTextViewDate = findViewById(R.id.add_appointment_text_view_date);
         mTextViewTime = findViewById(R.id.add_appointment_text_view_time);
+        mEditTextName = findViewById(R.id.add_appointment_edit_text_name);
+        mSpinnerTaskType = findViewById(R.id.add_appointment_spinner_task_type);
     }
 
     private void setCurrentDateAndTime() {
@@ -111,6 +120,71 @@ public class AddAppointmentActivity extends AppCompatActivity {
     }
 
     public void onAddAppointmentButton(View view) {
+        String name = mEditTextName.getText().toString();
+        String taskType = mSpinnerTaskType.getSelectedItem().toString();
+
+        if (!TextUtils.isEmpty(name)){
+            Intent intent = new Intent();
+            intent.putExtra("name", name);
+            intent.putExtra("type", taskType);
+
+            intent.putExtra("monthOfYear", DisplayTheMonthInString(mMonth));
+            intent.putExtra("dayOfMonth", mDay);
+            intent.putExtra("year", mYear);
+
+            intent.putExtra("hour", formatTheHour(mHour));
+            intent.putExtra("minute", mMinute);
+            intent.putExtra("AMorPM", amOrPm(mHour));
+
+            setResult(RESULT_OK, intent);
+            finish();
+        }else{
+            Toast.makeText(this, "Fill Task Name Please", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String amOrPm(final int hour) {
+        if (hour > 12)
+            return "PM";
+        return "AM";
+    }
+
+    private int formatTheHour(int hour) {
+        if (hour > 12){
+            hour -= 12;
+        }
+        return hour;
+    }
+
+    private String DisplayTheMonthInString(final int month) {
+        switch (month){
+            case 0:
+                return "Jan";
+            case 1:
+                return "Feb";
+            case 2:
+                return "Mar";
+            case 3:
+                return "Apr";
+            case 4:
+                return "May";
+            case 5:
+                return "Jun";
+            case 6:
+                return "Jul";
+            case 7:
+                return "Aug";
+            case 8:
+                return "Sep";
+            case 9:
+                return "Oct";
+            case 10:
+                return "Nov";
+            case 11:
+                return "Dec";
+        }
+
+        return "";
     }
 
     public void onCancelButton(View view) {
