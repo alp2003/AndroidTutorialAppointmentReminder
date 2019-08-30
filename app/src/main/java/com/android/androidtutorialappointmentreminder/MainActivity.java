9f024c290b7e6b14488d8937757d18847760a00b
Alex Pekker <alp2003@gmail.com>
2019-08-30 20:33:49 +0300

    Added onActivityResult

--- app/src/main/java/com/android/androidtutorialappointmentreminder/MainActivity.java
+++ app/src/main/java/com/android/androidtutorialappointmentreminder/MainActivity.java
@@ -2,10 +2,15 @@ package com.android.androidtutorialappointmentreminder;
 
 import android.content.Intent;
 import android.view.View;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE_ADD_APPOINTMENT = 999;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void onAddAppointment(View view) {
         startActivity(new Intent(this, AddAppointmentActivity.class));
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, @Nullable final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_APPOINTMENT && resultCode == RESULT_OK){
+
+        }
     }
 }
